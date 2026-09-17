167a11b4fe5dc554903e911810b61ccffd9195da
Wall-ES2 <willycasal789@gmail.com>
2026-09-17 20:51:44 -0300

    Updates

--- src/components/PageHeader.jsx
+++
@@ -1,9 +0,0 @@
-export function PageHeader({ title, description }) {
-  return (
-    <div className="page-heading">
-      <p className="eyebrow">PÁGINA</p>
-      <h1>{title}</h1>
-      <p className="page-description">{description}</p>
-    </div>
-  )
-}
